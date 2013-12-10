6c598054709bf1ae3785e8560cc6a1a3f58a7b43
oldrich.kepka@gmail.com <oldrich.kepka@gmail.com>
2013-12-10 23:13:13 +0100

    update

--- grabber.registry/src/main/java/org/okep/grabber/registry/internal/RegistryImpl.java
+++ grabber.registry/src/main/java/org/okep/grabber/registry/internal/RegistryImpl.java
@@ -2,11 +2,17 @@ package org.okep.grabber.registry.internal;
 
 import org.okep.grabber.registry.Grabber;
 import org.okep.grabber.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.TaskScheduler;
 
 import java.util.Set;
 
 public class RegistryImpl implements Registry {
 
+    private static final Logger log = LoggerFactory.getLogger(RegistryImpl.class);
+
+    private TaskScheduler taskScheduler;
 
     Set<Grabber> grabbers;
 
@@ -16,5 +22,13 @@ public class RegistryImpl implements Registry {
 
     public void setGrabbers(Set<Grabber> grabbers) {
         this.grabbers = grabbers;
+    }
+
+    public TaskScheduler getTaskScheduler() {
+        return taskScheduler;
+    }
+
+    public void setTaskScheduler(TaskScheduler taskScheduler) {
+        this.taskScheduler = taskScheduler;
     }
 }
