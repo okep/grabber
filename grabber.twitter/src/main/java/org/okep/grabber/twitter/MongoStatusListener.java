d005dc34520e335f7adbc87f888fb1a730242896
oldrich.kepka@gmail.com <oldrich.kepka@gmail.com>
2013-12-14 13:21:55 +0100

    twitter grabber implemented

--- grabber.twitter/src/main/java/org/okep/grabber/twitter/MongoStatusListener.java
+++ grabber.twitter/src/main/java/org/okep/grabber/twitter/MongoStatusListener.java
@@ -1,4 +1,71 @@
 package org.okep.grabber.twitter;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+import org.okep.grabber.persistence.MongoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.json.DataObjectFactory;
+
 public class MongoStatusListener implements StatusListener {
+
+    private static final Logger log = LoggerFactory.getLogger(MongoStatusListener.class);
+
+    private MongoClient mongoClient;
+    private static final String GRABBER_TWITTER_COLLECTION = "grabber_twitter";
+    private static final String GRABBER_TWITTER_DELETE_COLLECTION = "grabber_delete_twitter";
+    private volatile int counter = 0;
+
+    @Override
+    public void onStatus(Status status) {
+        String rawJSON = DataObjectFactory.getRawJSON(status);
+        mongoClient.getCollection(GRABBER_TWITTER_COLLECTION).insert(mongoClient.addTime((BasicDBObject) JSON.parse(rawJSON)));
+        counter++;
+    }
+
+    @Override
+    public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+        log.debug("Deleting status {}", statusDeletionNotice.getStatusId());
+        BasicDBObject delete = new BasicDBObject();
+        delete.append("statusId", statusDeletionNotice.getStatusId());
+        delete.append("userId", statusDeletionNotice.getUserId());
+        mongoClient.getCollection(GRABBER_TWITTER_DELETE_COLLECTION).insert(mongoClient.addTime(delete));
+    }
+
+    @Override
+    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+        log.error("Track limitation notice, number of limited statuses {}", numberOfLimitedStatuses);
+    }
+
+    @Override
+    public void onScrubGeo(long l, long l2) {
+        log.info("onScrubGeo");
+    }
+
+    @Override
+    public void onStallWarning(StallWarning stallWarning) {
+        log.warn(stallWarning.toString());
+    }
+
+    @Override
+    public void onException(Exception e) {
+        log.error("Streem exception", e);
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public void setMongoClient(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
 }
--- grabber.twitter/src/main/java/org/okep/grabber/twitter/TwitterGrabber.java
+++ grabber.twitter/src/main/java/org/okep/grabber/twitter/TwitterGrabber.java
@@ -3,27 +3,84 @@ package org.okep.grabber.twitter;
 import org.okep.grabber.persistence.MongoClient;
 import org.okep.grabber.registry.Grabber;
 import org.okep.grabber.registry.GrabberStatistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.Set;
+import java.util.*;
 
 public class TwitterGrabber implements Grabber {
 
+    private static final String TRACK_PARAM = "twitter.track";
+    private static final String NAME = "twitter_grabber";
+    private static final String DISPLAY_NAME = "Twitter Grabber";
+
+    private static final Logger log = LoggerFactory.getLogger(TwitterGrabber.class);
+
     private MongoClient mongoClient;
-    private Set<String> track;
+    private List<String> track;
+    private MongoStatusListener mongoStatusListener;
+    private String consumerKey;
+    private String consumerSecret;
+    private String token;
+    private String tokenSecret;
+    private String user;
+    private TwitterStream twitterStream;
+    TwitterStatistics twitterStatistics;
 
     @Override
     public GrabberStatistics getStatistic() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return twitterStatistics;
     }
 
     @Override
     public String getName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return NAME;
     }
 
     @Override
     public String getDisplayName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return DISPLAY_NAME;
+    }
+
+    public void serviceRegistered(TwitterGrabber grabber, Map params) {
+        String t = (String) params.get(TRACK_PARAM);
+        if(t != null) {
+            String[] split = t.split(",");
+            this.track = Arrays.asList(split);
+        }
+        log.info("Starting TwitterGrabber with track: " + track);
+
+        start();
+
+    }
+
+    public void start() {
+        twitterStatistics = new TwitterStatistics(new Date());
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setJSONStoreEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(token)
+                .setOAuthAccessTokenSecret(tokenSecret);
+
+        twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+
+        twitterStream.addListener(mongoStatusListener);
+        log.debug("Tracking {}", track);
+        String[] trackArray = track.toArray(new String[track.size()]);
+        twitterStream.filter(new FilterQuery(0, new long[0], trackArray));
+    }
+
+    public void stop() {
+        twitterStream.shutdown();
+    }
+
+    public void serviceUnregisterd(TwitterGrabber grabber, Map params) {
+        log.info("TwitterGrabber is about to finish.");
     }
 
     public MongoClient getMongoClient() {
@@ -34,11 +91,70 @@ public class TwitterGrabber implements Grabber {
         this.mongoClient = mongoClient;
     }
 
-    public Set<String> getTrack() {
+    public List<String> getTrack() {
         return track;
     }
 
-    public void setTrack(Set<String> track) {
+    public void setTrack(List<String> track) {
         this.track = track;
+    }
+
+    public MongoStatusListener getMongoStatusListener() {
+        return mongoStatusListener;
+    }
+
+    public void setMongoStatusListener(MongoStatusListener mongoStatusListener) {
+        this.mongoStatusListener = mongoStatusListener;
+    }
+
+    public String getConsumerKey() {
+        return consumerKey;
+    }
+
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenSecret() {
+        return tokenSecret;
+    }
+
+    public void setTokenSecret(String tokenSecret) {
+        this.tokenSecret = tokenSecret;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    private class TwitterStatistics extends GrabberStatistics  {
+        private TwitterStatistics(Date started) {
+            super(started);
+        }
+
+        @Override
+        public String getStringRepresentation() {
+            return getDisplayName() + " started " + getStarted() + " go " + mongoStatusListener.getCounter() + " statuses";
+        }
     }
 }
